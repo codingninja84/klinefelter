472fe18d2a18cafc984df12198ec6ec380df50fb
Rich Hanna <codingninja84@gmail.com>
2017-05-13 08:38:50 -0700

    updated logo and hero section

--- public/src/app/main-content/main-content.component.ts
+++ public/src/app/main-content/main-content.component.ts
@@ -15,17 +15,47 @@ import { Component,  OnInit, trigger, state, style, transition, animate } from '
      transition('inactive => active', animate('500ms ease-in-out')),
      transition('active => inactive', animate('100ms ease-in-out')),
  ]),
+ trigger('triangleLeft', [
+  state('inactive', style({
+    borderRight: "35vw solid transparent",
+    borderBottom: "0vw solid #f2f2f2",
+    borderLeft: "20vw solid transparent"
+  })),
+  state('active', style({
+    borderRight: "35vw solid transparent",
+    borderBottom: "10vw solid #f2f2f2",
+    borderLeft: "20vw solid transparent"
+  })),
+  transition('inactive => active', animate('1000ms ease-in-out')),
+  transition('active => inactive', animate('100ms ease-in-out')),
+]),
+trigger('triangleRight', [
+ state('inactive', style({
+   borderRight: "16vw solid transparent",
+   borderBottom: "0vw solid #f2f2f2",
+   borderLeft: "27vw solid transparent"
+ })),
+ state('active', style({
+   borderRight: "16vw solid transparent",
+   borderBottom: "8vw solid #f2f2f2",
+   borderLeft: "27vw solid transparent"
+ })),
+ transition('inactive => active', animate('1000ms 400ms ease-in-out')),
+ transition('active => inactive', animate('100ms ease-in-out')),
+]),
  ]
 })
 export class MainContentComponent implements OnInit {
-  referState = "inactive"
+  referState = "inactive";
+  triState = "inactive";
   constructor() { }
 
   ngOnInit() {
   }
-  
+
   toggleRefer(){
     this.referState = this.referState == "inactive" ? 'active' : 'inactive'
+    this.triState = "active"
   }
 
 }
