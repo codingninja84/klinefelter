6495decb3e431f9966837acda24eb7faa01b3a9e
Ian Tseng <ianvtseng@gmail.com>
2017-05-25 16:44:03 -0700

    adding testing scripts

---
+++ test/first_test.js
@@ -0,0 +1,33 @@
+var chai = require("chai");
+var expect = chai.expect;
+
+describe("Mocha", function () {
+    it("runs correctly", function () {
+        expect("hello").to.equal("hello");
+        expect("hello").to.not.equal("goodbye");
+        expect(24).to.be.below(25);
+        expect(24).to.be.at.least(23);
+    });
+});
+
+/* Inside first_test.js */
+// var assert = require("assert");
+// describe("Mocha", function () {
+//     it("runs correctly", function () {
+//         // This will fail tests if not true
+//         // (try changing it to false, see what happens)
+//         assert.equal("hello", "hello");
+//     });
+// });
+//
+// describe("the car", function () {
+//     it("has 4 wheels", function () {
+//          // Tests go here
+//     });
+//     it("can accelerate from 0mph to 60mph in less than 6 seconds", function () {
+//         // Tests go here
+//     });
+//     it("deploys an airbag in an accident", function () {
+//         // Tests go here
+//     });
+// });
