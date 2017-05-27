db2032a003943558657a8b33df4db725364e4dd7
Ian Tseng <ianvtseng@gmail.com>
2017-05-26 17:49:02 -0700

    pushing up final changes for the day, small tweaks to test script. Still working on some bugs

--- test/tester.js
+++ test/tester.js
@@ -20,32 +20,32 @@ describe("Klinefelter-Insurance Auto Quote Form Test", function () {
     this.timeout(100000);
     var d = this.driver;
     d.get("localhost:8000").then(function () {
-      d.findElement(by.css("p")).getText().then(function (titleText) {
-        expect(titleText).to.include("950 Pacific Ave Ste 640 Tacoma, Wa 98402");
-        d.findElement(by.id("autoQuote")).click();
-        d.findElement(by.name("first_name")).sendKeys("Ian");
-        d.findElement(by.name("last_name")).sendKeys("Tseng");
-        d.findElement(by.name("address")).sendKeys("5121 26th Ave NE");
-        d.findElement(by.name("apt_num")).sendKeys("B");
-        d.findElement(by.name("city")).sendKeys("Seattle");
-        d.findElement(by.name("state")).sendKeys("WA");
-        setTimeout(function() {
-          d.findElement(by.id("next1")).click();
-        }, 2000);
-        setTimeout(function() {
-          d.findElement(by.id("next2")).click();
-        }, 5000);
-        setTimeout(function() {
-          d.findElement(by.id("next3")).click();
-        }, 7000);
-        setTimeout(function() {
-          d.findElement(by.id("submit1")).click();
-        }, 9000);
-        setTimeout(function() {
-          d.findElement(by.id("close1")).click();
-        }, 11000);
-        done();
-      });
+      d.findElement(by.id("autoQuote")).click();
+      d.findElement(by.name("first_name")).sendKeys("Ian");
+      d.findElement(by.name("last_name")).sendKeys("Tseng");
+      d.findElement(by.name("address")).sendKeys("5121 26th Ave NE");
+      d.findElement(by.name("apt_num")).sendKeys("B");
+      d.findElement(by.name("city")).sendKeys("Seattle");
+      d.findElement(by.name("state")).sendKeys("WA");
+      setTimeout(function() {
+        d.findElement(by.id("next1")).click();
+      }, 2000);
+      setTimeout(function() {
+        d.findElement(by.id("next2")).click();
+      }, 5000);
+      setTimeout(function() {
+        d.findElement(by.id("next3")).click();
+      }, 7000);
+      setTimeout(function() {
+        d.findElement(by.id("submit1")).click();
+      }, 9000);
+      setTimeout(function() {
+        d.findElement(by.id("close1")).click();
+      }, 11000);
+      // d.findElement(by.id("successMessage")).getText().then(function (success) {
+      //   expect(success).to.include("We will contact you shortly with a quote from one of our carriers");
+      //   done();
+      // });
     });
   });
 });
